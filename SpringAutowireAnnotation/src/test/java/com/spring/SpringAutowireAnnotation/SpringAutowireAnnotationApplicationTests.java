4956b99913df578e563c674a33e0be3f959ffac6
@T0s1n <famorotitosin2010@yahoo.com>
2020-05-02 13:51:02 +0100

    2nd set first spring project upload

---
+++ SpringAutowireAnnotation/src/test/java/com/spring/SpringAutowireAnnotation/SpringAutowireAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringAutowireAnnotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAutowireAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
